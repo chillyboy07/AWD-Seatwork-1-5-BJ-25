78b5d5bbd95cd2ab1577448726abd5cf12d23600
chillyboy07 <bonneviechilly@gmail.com>
2025-02-05 18:05:39 +0800

    feat(js page 2): added reserve seat function

--- pages/page2/assets/js/scripts.js
+++ pages/page2/assets/js/scripts.js
@@ -4,4 +4,15 @@ let seats = {
     bus3: [true, true, true, true, true]
 };
 
+function reserveSeat() {
+    let userName = document.getElementById("name-input").value.trim();
+    let selectedBus = document.getElementById("bus-select").value;
+    let selectedSeat = document.getElementById("seat-select").value;
 
+    // if name is empty
+    if (userName === "") {
+        alert("Please enter your name before reserving a seat.");
+        return;
+    }
+
+}
